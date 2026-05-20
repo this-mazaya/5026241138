ae0763f9b81cc25b8a25ce52cc2435f91105ab0c
this-mazaya <erfindrimazaya@gmail.com>
2026-05-20 16:06:32 +0700

    pertemuan 9

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\MenuController;
 
 Route::get('/', function () {
@@ -18,6 +20,14 @@ Route::get('blog', function() {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
 Route::get('menu', [MenuController::class, 'index']);
 Route::get('contoh', [MenuController::class, 'contoh']);
